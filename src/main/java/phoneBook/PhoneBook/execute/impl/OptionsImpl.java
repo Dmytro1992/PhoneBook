280e759c21405a7e229d774c3bab4ffb44fc0077
Dmytro1992 <dishumov@yandex.ru>
2018-08-29 00:42:00 +0300

    realization of function

---
+++ src/main/java/phoneBook/PhoneBook/execute/impl/OptionsImpl.java
@@ -0,0 +1,50 @@
+package phoneBook.PhoneBook.execute.impl;
+
+import java.util.List;
+
+import phoneBook.PhoneBook.execute.Options;
+import phoneBook.PhoneBook.model.User;
+
+public class OptionsImpl implements Options{
+
+	public void add(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<User> showAllUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> searchByFirstName(String firstName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> searchByLastName(String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> searchBySurname(String surname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> searchByNumber(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> searchByAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
