8409289c2053c3485b4d1ea871f96ebd82cf3a20
Dmytro1992 <dishumov@yandex.ru>
2018-08-29 00:34:21 +0300

    User model

---
+++ src/main/java/phoneBook/PhoneBook/model/User.java
@@ -0,0 +1,55 @@
+package phoneBook.PhoneBook.model;
+
+public class User {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String surname;
+	private int number;
+	private String address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "PhoneBook [id="+id+", firstName=" + firstName + ", lastName=" + lastName + ", surname=" + surname + ", number="
+				+ number + ", address=" + address + "]";
+	}
+	
+	
+}
