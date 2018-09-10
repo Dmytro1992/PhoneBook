93d9675304eb5892bd5f431ba30b66a36b8f7472
Dmytro1992 <dishumov@yandex.ru>
2018-09-10 22:09:56 +0300

    Get session

---
+++ src/main/java/phoneBook/PhoneBook/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package phoneBook.PhoneBook.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory ourSessionFactory;
+	
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			
+			ourSessionFactory = configuration.buildSessionFactory();
+		}catch (Throwable e) {
+			e.getMessage();
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static Session getSession() throws HibernateException{
+		return ourSessionFactory.openSession();
+	}
+	
+	public static void closeSession() throws HibernateException{
+		 ourSessionFactory.close();
+	}
+}
